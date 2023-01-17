a52e4c4ac40e518dfb53ce08e192d20d4e31b97e
Zhanybek Sadvakassov <tbdttb@gmail.com>
2023-01-17 21:46:37 +0100

    Adding request input

--- src/services/pubsub.ts
+++ src/services/pubsub.ts
@@ -2,7 +2,7 @@ import { PubSub } from '@google-cloud/pubsub'
 import { config } from '../config'
 import { Proposal } from './snapshot'
 
-export const queuePrpoposals = async (proposals: Proposal[]) => {
+export const queueProposals = async (proposals: Proposal[]) => {
   const pubsub = new PubSub()
   const topic = pubsub.topic(config.pubsub.topicNameOrId)
   for (const proposal of proposals) {
